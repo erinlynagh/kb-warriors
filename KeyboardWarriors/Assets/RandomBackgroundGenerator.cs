3e83e97b3d40f46056d4b96c8771f91e6d84b737
shiy12 <809297016@qq.com>
2019-04-05 23:58:25 -0400

    Now able to randomly generate stages

---
+++ KeyboardWarriors/Assets/RandomBackgroundGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBackgroundGenerator : MonoBehaviour
+{
+
+    public Sprite[] Backgrounds; //this is an array of type sprite
+
+    public SpriteRenderer Render; //this is a variable of type sprite renderer
+
+    // Use this for initialization
+    void Start()
+    {
+        Render = GetComponent<SpriteRenderer>();
+        Render.sprite = Backgrounds[Random.Range(0, Backgrounds.Length)];
+    }
+    void Update()
+    {
+
+    }
+}
