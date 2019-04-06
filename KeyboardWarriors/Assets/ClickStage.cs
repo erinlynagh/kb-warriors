77fa979d5477a86b6e9d6a2324532982da46670e
shiy12 <809297016@qq.com>
2019-04-05 21:42:03 -0400

    Adding StageController and transition from stage to game

---
+++ KeyboardWarriors/Assets/ClickStage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickStage : MonoBehaviour
+{
+    private void OnMouseUp()
+    {
+        SceneManager.LoadScene("GameScreen");
+    }
+}
---
+++ KeyboardWarriors/Assets/StageGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageGenerator : MonoBehaviour
+{
+    public GameObject[] objects;
+    public Vector3[] Level1Spots;
+
+    void Start()
+    {
+        int rand = Random.Range(0,3);
+        Instantiate(objects[0], Level1Spots[rand], Quaternion.identity);
+    }
+
+    void Update()
+    {
+        
+    }
+}
