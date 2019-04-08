aae85cf3a431e9b876097f620be8d1d67726ace6
zhou7102 <32040029+zhou7102@users.noreply.github.com>
2019-04-07 22:42:07 -0400

    add score history

---
+++ KeyboardWarriors/Assets/Script/ScoreHistory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreHistory : MonoBehaviour
+{
+    private List<Score> records = DashboardIO.readScoresFromFile();
+
+    public List<Score> getRecord()
+    {
+        return records;
+    }
+
+    public void addScore(Score record)
+    {
+        if (records.Count == 0 || record.score > records[records.Count - 1].score)
+        {
+            records.Add(record);
+        }
+        else {
+            for (int i = 0; i < records.Count; i++)
+            {
+                if (record.score <= records[records.Count - 1].score)
+                {
+                    records.Insert(i, record);
+                }
+            }
+        }
+
+        DashboardIO.writeScoresToFile(records);
+    }
+
+    public void removeAll() {
+        records = new List<Score>();
+        DashboardIO.writeScoresToFile(records);
+    }
+}
